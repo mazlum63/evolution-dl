b2faff26b8d2921b8caf451fe1ee124ad9ffe62f
mazlum63 <mazlumkoyuncu63@gmail.com>
2025-06-24 13:34:52 +0200

    Update: animal energy methods

--- src/animal/animal-energy.ts
+++ src/animal/animal-energy.ts
@@ -1,25 +1,70 @@
 export class Energy {
-    energyUseageIdle = 0.2 / 60;
-    energyUseageMoving = 1 / 60;
-    energy = 10;
-    maxEnergy = 100;
-    durability = 10;
-    energyPerEat = 30;
+    private energyUseageIdle = 0.2 / 60;
+    private energyUseageMoving = 1 / 60;
+    private energy = 10;
+    private maxEnergy = 100;
+    private stamina = 10;
+    private energyPerEat = 30;
+    normalizedEnergy = this.energy / this.maxEnergy;
+    normalizedstamina = this.stamina / this.maxEnergy;
+    private staminaPercentage = 0.7;
 
     canSurvive(): boolean {
-        return this.energy > 0 && this.durability > 0
+        return this.energy > 0 || this.stamina > 0
     }
 
     decreaseEnergyIdle() {
+        if (this.energy == 0 && this.stamina == 0) {
+            return;
+        }
+        this.handleMinMaxValue();
         if (this.energy > 0) {
             this.energy -= this.energyUseageIdle;
         }
-        if (this.energy <= 0 && this.durability > 0) {
-            this.durability -= this.energyUseageIdle;
+        if (this.energy <= 0 && this.stamina > 0) {
+            this.stamina -= this.energyUseageIdle;
         }
     }
 
     decreaseEnergyMoving() {
-        this.energy -= this.energyUseageMoving;
+        if (this.energy == 0 && this.stamina == 0) {
+            return;
+        }
+        this.handleMinMaxValue();
+        if (this.energy > 0) {
+            this.energy -= this.energyUseageMoving;
+        }
+        if (this.energy <= 0 && this.stamina > 0) {
+            this.stamina -= this.energyUseageMoving;
+        }
+    }
+
+    increaseEnery() {
+        if (this.energy == this.maxEnergy && this.stamina == this.maxEnergy) {
+            return;
+        }
+        this.handleMinMaxValue();
+        if (this.energy < this.maxEnergy) {
+            this.energy += this.energyPerEat;
+        }
+        if (this.energy == this.maxEnergy && this.stamina < this.maxEnergy) {
+            this.stamina += this.energyPerEat * this.staminaPercentage;
+        }
+    }
+
+    private handleMinMaxValue() {
+        if (this.energy < 0) {
+            this.energy = 0;
+        }
+        if (this.stamina < 0) {
+            this.stamina = 0;
+        }
+
+        if (this.energy > this.maxEnergy) {
+            this.energy = this.maxEnergy;
+        }
+        if (this.stamina > this.maxEnergy) {
+            this.stamina = this.maxEnergy;
+        }
     }
 }
